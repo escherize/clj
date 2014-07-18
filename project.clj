2eb5c98d08037bf8e82602f6ae539d236df889b9
escherizer <bryan.maass@gmail.com>
2014-07-17 17:36:00 -0700

    updated cider to 0.7.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -7,4 +7,5 @@
                  [enlive                         "1.1.5"]
                  [hiccup                         "1.0.5"]
                  [org.clojure/math.combinatorics "0.0.7"]
-                 [dorothy                        "0.0.5"]])
+                 [dorothy                        "0.0.5"]]
+  :plugins [[cider/cider-nrepl "0.7.0-SNAPSHOT"]])
--- src/playground/apple.clj
+++ src/playground/apple.clj
@@ -1,14 +1,15 @@
-(ns playground.apple)
-
-(use 'clojure.string)
-
-(->> (slurp "http://shakespeare.mit.edu/midsummer/full.html")
-     split-lines
-     (map #(split % #"\s+|<|>") )
-     flatten
-     frequencies
-     seq
-     (sort-by second))
+(ns playground.apple
+  [:require [clojure.string :as str]])
 
 
+(defn word-count [path]
+  (->> (slurp )
+       split-lines
+       (mapcat #(split % #"\s+|<|>") )
+       frequencies
+       (sort-by second)))
+
+(comment
+  (word-count "http://shakespeare.mit.edu/midsummer/full.html")
+)
 
