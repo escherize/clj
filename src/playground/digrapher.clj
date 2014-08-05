f88b9ebbb16f7eb00af82fd2d48689de9388ad00
escherizer <bryan.maass@gmail.com>
2014-08-05 13:45:14 -0700

    update

--- src/playground/digrapher.clj
+++ src/playground/digrapher.clj
@@ -8,10 +8,12 @@
       dot
       (save! "out.png" {:format :png})))
 
-(def letters [:a :b :c :d :e])
-
-
-(digrapher [[:farm :apple :pie :mouth]])
+(defn grapher [edge-lists]
+  (-> edge-lists
+      (into [])
+      (graph)
+      dot
+      (save! "out.png" {:format :png})))
 
 (digrapher [[:start                 :token-acquired              {:label 100} ]
             [:token-acquired        :idle                        {:label 100} ]
@@ -35,60 +37,60 @@
             [:session-end           :start                       {:label 50} ]
             [:session-end           :halt                        {:label 50} ]])
 
-(comment  ; program flow
-  (digrapher [[:bucket :purchase-stats]
-              [:purchases-by-bucket :purchase-stats]
-              [:sessions-by-bucket :managed-sessions-by-bucket]
-              [:sessions-by-bucket :unmanaged-sessions-by-bucket]
-              [:managed-sessions-by-bucket :managed-session-stats]
-              [:bucket :managed-session-stats]
-              [:unmanaged-sessions-by-bucket :unmanaged-session-stats]
-              [:bucket :unmanaged-session-stats]
-              [:purchase-stats :sales-$]
-              [:purchase-stats :discounts-$]
-              [:purchase-stats :num-purchases]
-              [:managed-session-stats :num-sessions]
-              [:unmanaged-session-stats :num-unmanaged]
-              [:sales-$ :aov]
-              [:num-purchases :aov]
-              [:num-purchases :conversion]
-              [:num-sessions :conversion]
-              [:discounts-$ :discounts-%]
-              [:sales-$ :discount-$]
-              [:bucket :type]
-              [:bucket :name]])
+;; (comment  ; program flow
+;;   (digrapher [[:bucket :purchase-stats]
+;;               [:purchases-by-bucket :purchase-stats]
+;;               [:sessions-by-bucket :managed-sessions-by-bucket]
+;;               [:sessions-by-bucket :unmanaged-sessions-by-bucket]
+;;               [:managed-sessions-by-bucket :managed-session-stats]
+;;               [:bucket :managed-session-stats]
+;;               [:unmanaged-sessions-by-bucket :unmanaged-session-stats]
+;;               [:bucket :unmanaged-session-stats]
+;;               [:purchase-stats :sales-$]
+;;               [:purchase-stats :discounts-$]
+;;               [:purchase-stats :num-purchases]
+;;               [:managed-session-stats :num-sessions]
+;;               [:unmanaged-session-stats :num-unmanaged]
+;;               [:sales-$ :aov]
+;;               [:num-purchases :aov]
+;;               [:num-purchases :conversion]
+;;               [:num-sessions :conversion]
+;;               [:discounts-$ :discounts-%]
+;;               [:sales-$ :discount-$]
+;;               [:bucket :type]
+;;               [:bucket :name]])
 
 
-  (digrapher [[:session-added-to-partition? {:shape :diamond}]
-              [:session-present? {:shape :diamond}]
-              [:device-present? {:shape :diamond}]
-              [:partition-present? {:shape :diamond}]
+;;   (digrapher [[:session-added-to-partition? {:shape :diamond}]
+;;               [:session-present? {:shape :diamond}]
+;;               [:device-present? {:shape :diamond}]
+;;               [:partition-present? {:shape :diamond}]
 
-              [:device-tracker-id {:shape :none}]
-              [:user-token {:shape :none}]
-              [:current-run-ids {:shape :none}]
+;;               [:device-tracker-id {:shape :none}]
+;;               [:user-token {:shape :none}]
+;;               [:current-run-ids {:shape :none}]
 
-              [:return {:shape :square}]
-              ;; edges
-              [:device-tracker-id :session-present?]
-              [:user-token :session-present?]
-              [:session-present?
-               :partition-present?
-               :session-added-to-partition?
-               :return {:label :yes}]
-              [:session-present?
-               :device-present?
-               :create-device {:label :no}]
-              [:current-run-ids :partition-present?]
-              [:partition-present?
-               :create-partition
-               :add-partition-to-runs
-               :add-device-to-partition
-               :add-session-to-partition
-               :return
-               {:label :no}]
-              [:session-added-to-partition?
-               :add-session-to-partition
-               {:label :no}]
-              [:create-device :create-session :partition-present?]
-              [:device-present? :create-session {:label :yes}]]))
+;;               [:return {:shape :square}]
+;;               ;; edges
+;;               [:device-tracker-id :session-present?]
+;;               [:user-token :session-present?]
+;;               [:session-present?
+;;                :partition-present?
+;;                :session-added-to-partition?
+;;                :return {:label :yes}]
+;;               [:session-present?
+;;                :device-present?
+;;                :create-device {:label :no}]
+;;               [:current-run-ids :partition-present?]
+;;               [:partition-present?
+;;                :create-partition
+;;                :add-partition-to-runs
+;;                :add-device-to-partition
+;;                :add-session-to-partition
+;;                :return
+;;                {:label :no}]
+;;               [:session-added-to-partition?
+;;                :add-session-to-partition
+;;                {:label :no}]
+;;               [:create-device :create-session :partition-present?]
+;;               [:device-present? :create-session {:label :yes}]]))
