af5511d7c39343e07b925557983c70f2151cba1c
Bryan Maass <bryan.maass@gmail.com>
2014-09-01 20:37:50 -0700

    update rent.clj

--- src/playground/rent.clj
+++ src/playground/rent.clj
@@ -7,25 +7,27 @@
   "takes a list of nums and finds the sum"
   (apply + nums))
 
+(defn add-area [room]
+  (assoc room :area (apply * (:dimensions room))))
+
 (defn rent []
   (let [total-sq-ft     1267
         price           3479
         utilities       275.12
-        price-sqft      (double (/ price total-sq-ft))
         rooms           (->> [{:title "jeff's room"  :headcount 3 :dimensions [15.25 11.25]}
                               {:title "bryan's room" :headcount 1 :dimensions [11.5   13.5]}
                               {:title "thaly's room" :headcount 1 :dimensions [12.5   9.75]}]
-                             (map #(assoc % :area (apply * (:dimensions %)))))
+                             (map add-area))
         private-area    (sum (map :area rooms))
-        common-area     (- total-sq-ft private-area)
         occupants       (sum (map :headcount rooms))
+        common-area     (- total-sq-ft private-area)
+        price-sqft      (double (/ price total-sq-ft))
         common-price    (-> common-area (* price-sqft) (/ occupants))
         common-util     (/ utilities occupants)]
     (->> rooms
-         (map #(assoc % :total-price-pp (-> (:area %)
-                                            (* price-sqft)
-                                            (/ (:headcount %))
-                                            (+ common-price)
-                                            (+ common-util)))))))
-
-
+         (map (fn [room]
+                assoc room :total-price-pp (-> (:area room)
+                                               (* price-sqft)
+                                               (/ (:headcount room))
+                                               (+ common-price)
+                                               (+ common-util)))))))
