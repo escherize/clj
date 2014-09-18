d6e9e85a3a971f75ea4f68e8b57f4f25500bf6c7
Bryan Maass <bryan.maass@gmail.com>
2014-09-17 19:59:04 -0700

    stuff

--- src/playground/digrapher.clj
+++ src/playground/digrapher.clj
@@ -15,53 +15,25 @@
       dot
       (save! "out.png" {:format :png})))
 
-(digrapher   
-  ;;start-state            end-state               weight  
-  [[:date                  :date                  {:label "Aug 11, 2014"}]
-   [:start                 :token-acquired        {:label "100"}]
-   [:token-acquired        :idle                  {:label "100"}]
-   [:idle                  :user-info-provided    {:label "10"}]
-   [:idle                  :collecting-info       {:label "10"}]
-   [:idle                  :displaying-promo      {:label "10"}]
-   [:idle                  :purchase-made         {:label "15"}]
-   [:idle                  :buckets-identified    {:label "50"}]
-   [:idle                  :session-end           {:label "5"}]
-   [:user-info-provided    :idle                  {:label "90"}]
-   [:user-info-provided    :session-end           {:label "10"}]
-   [:collecting-info       :idle                  {:label "90"}]
-   [:collecting-info       :session-end           {:label "10"}]
-   [:displaying-promo      :idle                  {:label "90"}]
-   [:displaying-promo      :session-end           {:label "10"}]
-   [:purchase-made         :session-end           {:label "100"}]
-   [:buckets-identified    :buckets-confirmed     {:label "95"}]
-   [:buckets-identified    :buckets-not-confirmed {:label "5"}]
-   [:buckets-confirmed     :idle                  {:label "100"}]
-   [:buckets-not-confirmed :idle                  {:label "100"}]
-   [:session-end           :start                 {:label "10"}]
-   [:session-end           :halt                  {:label "90"}]])
+(digrapher  [[:start                 :token-acquired              {:label 100} ]
+             [:token-acquired        :idle                        {:label 100} ]
+             [:idle                  :user-info-provided          {:label 10} ]
+             [:idle                  :collecting-info             {:label 10} ]
+             [:idle                  :displaying-promo            {:label 10} ]
+             [:idle                  :purchase-made               {:label 15} ]
+             [:idle                  :buckets-identified          {:label 50} ]
+             [:idle                  :session-end                 {:label 05} ]
+             [:user-info-provided    :idle                        {:label 90} ]
+             [:user-info-provided    :session-end                 {:label 10} ]
+             [:collecting-info       :idle                        {:label 90} ]
+             [:collecting-info       :session-end                 {:label 10} ]
+             [:displaying-promo      :idle                        {:label 90} ]
+             [:displaying-promo      :session-end                 {:label 10} ]
+             [:purchase-made         :session-end                 {:label 100} ]
+             [:buckets-identified    :buckets-confirmed           {:label 100} ]
+             [:buckets-confirmed     :idle                        {:label 100} ]
+             [:session-end           :start                        {:label 10}]
+             [:session-end           :halt {:label 90} ]])
 
-(comment
-  (digrapher
-   [[:start                 :token-acquired     {:label 100}]
-    [:token-acquired        :idle               {:label 100}]
-    [:idle                  :user-info-provided {:label 10}]
-    [:idle                  :collecting-info    {:label 10}]
-    [:idle                  :displaying-promo   {:label 10}]
-    [:idle                  :purchase-made      {:label 15}]
-    [:idle                  :buckets-identified {:label 50}]
-    [:idle                  :session-end        {:label 05}]
-    [:user-info-provided    :idle               {:label 90}]
-    [:user-info-provided    :session-end        {:label 10}]
-    [:collecting-info       :idle               {:label 90}]
-    [:collecting-info       :session-end        {:label 10}]
-    [:displaying-promo      :idle               {:label 90}]
-    [:displaying-promo      :session-end        {:label 10}]
-    [:purchase-made         :session-end        {:label 100}]
-    [:buckets-identified    :buckets-confirmed  {:label 100}]
-    [:buckets-confirmed     :idle               {:label 100}]
-    [:buckets-not-confirmed :idle               {:label 100}]
-    [:session-end           :start              {:label 95}]
-    [:session-end           :halt               {:label 5}]])
-  
-)
+
 
--- src/playground/rent.clj
+++ src/playground/rent.clj
@@ -1,33 +1,24 @@
-(ns playground.rent
-  [:require [clojure.string :as str]
-   [print.foo :refer :all]
-   [clojure.pprint :refer :all]])
-
-(defn sum [nums]
-  "takes a list of nums and finds the sum"
-  (apply + nums))
-
-(defn add-area [room]
-  (assoc room :area (apply * (:dimensions room))))
+(ns playground.rent)
 
 (defn rent []
-  (let [total-sq-ft     1267
-        price           3479
-        utilities       275.12
-        rooms           (->> [{:title "jeff's room"  :headcount 3 :dimensions [15.25 11.25]}
-                              {:title "bryan's room" :headcount 1 :dimensions [11.5   13.5]}
-                              {:title "thaly's room" :headcount 1 :dimensions [12.5   9.75]}]
-                             (map add-area))
-        private-area    (sum (map :area rooms))
-        occupants       (sum (map :headcount rooms))
-        common-area     (- total-sq-ft private-area)
-        price-sqft      (double (/ price total-sq-ft))
-        common-price    (-> common-area (* price-sqft) (/ occupants))
-        common-util     (/ utilities occupants)]
-    (->> rooms
-         (map (fn [room]
-                assoc room :total-price-pp (-> (:area room)
-                                               (* price-sqft)
-                                               (/ (:headcount room))
-                                               (+ common-price)
-                                               (+ common-util)))))))
+  (let [total-sq-ft  1267
+        price        3479
+        utilities    275.12
+        rooms        [{:title "jeff's room"  :headcount 3 :area 171.5625}
+                      {:title "bryan's room" :headcount 1 :area 155.25}  
+                      {:title "thaly's room" :headcount 1 :area 121.875}]
+        private-area (apply + (map :area rooms))
+        occupants    (apply + (map :headcount rooms))
+        common-area  (- total-sq-ft private-area)
+        price-sqft   (double (/ price total-sq-ft))
+        common-price (-> common-area (* price-sqft) (/ occupants))
+        common-util  (/ utilities occupants)]
+    (map (fn [room]
+           (assoc room :total-price-pp (-> (:area room)
+                                           (* price-sqft)
+                                           (/ (:headcount room))
+                                           (+ common-price)
+                                           (+ common-util))))
+         rooms)))
+
+(rent 1267 3479 275.12)
